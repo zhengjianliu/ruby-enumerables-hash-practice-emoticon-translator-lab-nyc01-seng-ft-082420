9840b5db5bc05657733787fd2450526f1989b3f6
ZHENGJIAN LIU <comezheng@gmail.com>
2020-08-14 22:40:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,9 +19,12 @@ def get_japanese_emoticon(path,emoticon)
 
   # code goes here
   data = YAML.load_file(path)
-  data.each_with_object({}) do |(icon, value),final_hash|
+  data.each_with_object({}) do |(meaning, value),final_hash|
   if emoticon == value
+    return meaning
+  end
   binding.pry
+end
 end
   
 end
